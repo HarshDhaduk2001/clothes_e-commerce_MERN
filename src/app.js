9a195850c1f5ac34729ec001525c13481230d33f
HarshDhaduk2001 <harshdhaduk2001@gmail.com>
2023-10-12 22:21:56 +0530

    routes created

--- src/app.js
+++ src/app.js
@@ -8,11 +8,27 @@ app.use(express.json());
 app.use(cors());
 dotenv.config();
 
+const adminProductRoute = require("./routes/adminProductRoute");
+const adminOrderRoute = require("./routes/adminOrderRoute");
 const authRouters = require("./routes/authRouter");
+const cartItemRoute = require("./routes/cartItemRoute");
+const cartRoute = require("./routes/cartRoute");
+const orderRoutes = require("./routes/orderRoutes");
+const productRoute = require("./routes/productRoute");
+const ratingRouter = require("./routes/ratingRouter");
+const reviewRoute = require("./routes/reviewRoute");
 const userRouters = require("./routes/userRouter");
 
 app.use("/auth", authRouters);
 app.use("/api/users", userRouters);
+app.use("/api/products", productRoute);
+app.use("/api/admin/products", adminProductRoute);
+app.use("/api/cart", cartRoute);
+app.use("/api/cart_items", cartItemRoute);
+app.use("/api/orders", orderRoutes);
+app.use("/api/admin/orders", adminOrderRoute);
+app.use("/api/reviews", reviewRoute);
+app.use("/api/ratings", ratingRouter);
 
 // MongoDB connection
 mongoose
---
+++ src/routes/adminOrderRoute.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const adminController = require("../controllers/adminOrderController");
+const authenticate = require("../middleware/authenticate");
+
+router.get("/", authenticate, adminController.getAllOrders);
+router.put("/:orderId/confirmed", authenticate, adminController.confirmOrders);
+router.put("/:orderId/ship", authenticate, adminController.shipOrders);
+router.put("/:orderId/deliver", authenticate, adminController.deliverOrders);
+router.put("/:orderId/cancel", authenticate, adminController.cancelledOrders);
+router.put("/:orderId/delete", authenticate, adminController.deleteOrders);
+
+module.exports = router;
