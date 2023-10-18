ba5c19395fbd0e429d6095a7e78cb6ed900b797d
HarshDhaduk2001 <harshdhaduk2001@gmail.com>
2023-10-18 22:11:47 +0530

    login and register ui completed

--- src/customer/routes/CustomerRoute.jsx
+++ src/customer/routes/CustomerRoute.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage/HomePage";
-import Cart from "../components/Cart/Cart"
-import Product from "../components/Product/Product"
-import ProductDetails from "../components/ProductDetails/ProductDetails"
-import Checkout from "../components/Checkout/Checkout"
-import Order from "../components/Order/Order"
-import OrderDetails from "../components/Order/OrderDetails"
+import Cart from "../components/Cart/Cart";
+import Product from "../components/Product/Product";
+import ProductDetails from "../components/ProductDetails/ProductDetails";
+import Checkout from "../components/Checkout/Checkout";
+import Order from "../components/Order/Order";
+import OrderDetails from "../components/Order/OrderDetails";
 
 const CustomerRoute = () => {
   return (
     <div>
       <Routes>
+        <Route path="/login" exact element={<HomePage />} />
+        <Route path="/register" exact element={<HomePage />} />
         <Route path="/" exact element={<HomePage />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/:lavelOne/:lavelTwo/:levelThree" element={<Product />} />
